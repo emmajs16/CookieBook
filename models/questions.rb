2cad8eb72d0b1cbfa2beee405e60e1e9e082c81b
Emma Stoverink <emmajs16@live.com>
2018-06-26 01:12:13 +0000

    deleted models and quiz

--- models/questions.rb
+++ models/questions.rb
@@ -1,9 +1,9 @@
 def cookie_chooser(user_value)
  if user_value <4
-   "cranberry"
+    "cranberry"
  elsif user_value <7
-   "trailmix"
+    "trailmix"
  elsif user_value <10
-   "chocolatey"
+    "chocolatey"
 end
 end
